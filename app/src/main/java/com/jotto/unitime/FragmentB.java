e4b9f5edc68d9aae69d5884f60e8dc4572e5963b
Johan Rovala <johanrovala@Jonass-MBP-2.lan>
2015-06-18 13:32:25 +0200

    added a few things, listview and calendarview to both fragments

--- app/src/main/java/com/jotto/unitime/FragmentB.java
+++ app/src/main/java/com/jotto/unitime/FragmentB.java
@@ -8,13 +8,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 /**
  * Created by johanrovala on 18/06/15.
  */
 public class FragmentB extends Fragment {
+    CalendarView calendarView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_b,container,false);
+        
     }
 }
