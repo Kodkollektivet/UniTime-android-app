06120da5e01fd93f0d1281d7890fb6c0632a043c
Johan Rovala <johanrovala@Jonass-MBP-2.lan>
2015-06-20 15:46:37 +0200

    added examples for changing colors of cells (dates) within caldroid

--- app/src/main/java/com/jotto/unitime/FragmentB.java
+++ app/src/main/java/com/jotto/unitime/FragmentB.java
@@ -14,13 +14,17 @@ import android.view.ViewGroup;
 
 import com.roomorama.caldroid.CaldroidFragment;
 
+import org.joda.time.DateTime;
+
 import java.util.Calendar;
+import java.util.Date;
 
 
 public class FragmentB extends Fragment {
 
     private FragmentActivity myContext;
     Calendar cal;
+    Date date;
 
     @Override
     public void onAttach(Activity activity) {
@@ -40,9 +44,34 @@ public class FragmentB extends Fragment {
         CaldroidFragment calDroid = new CaldroidFragment();
         Bundle args = new Bundle();
         cal = Calendar.getInstance();
+        date = new Date(System.currentTimeMillis());
+        calDroid.setMinDate(date);
         args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
         args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
         calDroid.setArguments(args);
+
+
+        /*
+        Testing different colored cells
+         */
+
+
+        // first date
+        DateTime dateTime = new DateTime(System.currentTimeMillis());
+        dateTime = dateTime.plusDays(3);
+
+        // second date
+        DateTime secondDateTime = new DateTime(System.currentTimeMillis());
+        secondDateTime = secondDateTime.plusDays(5);
+
+        // third date
+        DateTime thirdDateTime = new DateTime(System.currentTimeMillis());
+        thirdDateTime = thirdDateTime.plusDays(6);
+
+        // assigning new backgroundresorces for three cells (dates)
+        calDroid.setBackgroundResourceForDate(R.color.blue, dateTime.toDate());
+        calDroid.setBackgroundResourceForDate(R.color.darkgreen, secondDateTime.toDate());
+        calDroid.setBackgroundResourceForDate(R.color.blue, thirdDateTime.toDate());
 
         android.support.v4.app.FragmentTransaction t = myContext
                 .getSupportFragmentManager().beginTransaction();
